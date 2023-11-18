9982254d57bc672764ba5afd302d2e1cd184a218
Hakony <anphamhkn@gmail.com>
2023-11-17 16:05:43 -0800

    init page

---
+++ HomePage.xaml.cs
@@ -0,0 +1,15 @@
+namespace SolarCarCoreUI;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
